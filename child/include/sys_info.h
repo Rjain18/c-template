3118bce113d753c025854810cf2964cdc04afa79
rushabh <jainrushabh1801@gmail.com>
2026-02-09 20:47:00 +0530

    System Info initial commit

---
+++ child/include/sys_info.h
@@ -0,0 +1,17 @@
+/**
+ * @file: sys_info.c
+ * @brief: Source file for sys_info module
+ */
+
+/*
+ * project: 01PROJTEMP
+ * module: sys_info
+ * created: 2026-02-09
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * author: Rushabh Jain
+ */
+
+#ifndef SYS_INFO_H
+#define SYS_INFO_H
+
+#endif // SYS_INFO_H
---
+++ child/src/sys_info.c
@@ -0,0 +1,12 @@
+/**
+ * @file: sys_info.c
+ * @brief: Source file for sys_info module
+ */
+
+/*
+ * project: 01PROJTEMP
+ * module: sys_info
+ * created: 2026-02-09
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * author: Rushabh Jain
+ */
