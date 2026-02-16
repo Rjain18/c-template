88d331e9b93e825dff22098962ba42ef68bdce68
rushabh <jainrushabh1801@gmail.com>
2026-02-16 21:13:51 +0530

    Updated invocation struct

--- child/include/invocation.h
+++ child/include/invocation.h
@@ -13,4 +13,25 @@
 #ifndef INVOCATION_H
 #define INVOCATION_H
 
+typedef struct 
+{
+    int argc;
+    char **argv;
+    char *cwd;    
+} invocation_t;
+
+/**
+ * @brief initializes the Invocation
+ * @param[in] argc argument count 
+ * @param[in] argv argument array
+ * @return pointer to invocation structure
+ */
+invocation_t* initInvocation(int argc, char *argv[]);  
+
+/**
+ * @brief Get current directory
+ * @return current directory path
+ */
+char* getCurrentDirectory();
+
 #endif // INVOCATION_H
--- child/src/invocation.c
+++ child/src/invocation.c
@@ -10,3 +10,23 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  * author: Rushabh Jain
  */
+
+#include "invocation.h"
+
+ invocation_t* initInvocation(int argc, char *argv[])
+ {
+    invocation_t *localInvocation = (invocation_t*)malloc(sizeof(invocation_t));
+
+    if(localInvocation != NULL)
+    {
+        //Store argc
+        localInvocation->argc = argc;
+
+        //Deep copy argv
+        localInvocation->argv = (char**)malloc((argc+1)* sizeof(char*));
+    }
+    else{
+        // Log the error 
+        return NULL;
+    }
+ }
